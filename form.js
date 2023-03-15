b4defc4971c3b2b7bebc4c2995e3fa993f053baa
Alberto <avelazquez@abogadoericprice.com>
2023-03-15 22:28:38 +0000

    First Commit

---
+++ form.js
@@ -0,0 +1,72 @@
+// const submitForms = () => {
+
+//     var requestDelay = 2000;
+
+//     document.getElementById("FormPhp").submit();
+// }
+
+
+const sendData = () => {
+    const firstNamePhp = document.getElementById("first_name").value;
+    const lastNamePhp = document.getElementById("last_name").value;
+    const emailPhp = document.getElementById("email").value;
+    const phonePhp = document.getElementById("mobile").value;
+    const messagePhp = document.getElementById("message").value;
+
+    $.ajax({
+        type: 'POST',
+        url: 'mail.php',
+        data: {
+            firstName: firstNamePhp,
+            lastName: lastNamePhp,
+            phone: phonePhp,
+            email: emailPhp,
+            message: messagePhp,
+
+        },
+        success: function () {
+            console.log("Enviado pa");
+        }
+    });
+}
+
+// #region GetInput Onclick
+
+// const submitSalesforce = () => {
+//     document.getElementById("formSalesforce").submit();
+//     document.getElementById("formSalesforce").submit();
+// }
+
+// const submitPhp = () => {
+//     document.getElementById("FormPhp").submit();
+//     document.getElementById("formSalesforce").submit();
+// }
+
+const getFirstName = () => {
+    const first_name = document.getElementById("first_name").value;
+    document.getElementById("first_namePhp").value = first_name;
+}
+
+const getLastName = () => {
+    const last_name = document.getElementById("last_name").value;
+    document.getElementById("last_namePhp").value = last_name;
+}
+
+const getEmail = () => {
+    const email = document.getElementById("email").value;
+    document.getElementById("emailPhp").value = email;
+}
+
+const getNumber = () => {
+    const mobile = document.getElementById("mobile").value;
+    document.getElementById("mobilePhp").value = mobile;
+}
+
+const getQuestion = () => {
+    const message = document.getElementById("message").value;
+    document.getElementById("messagePhp").value = message;
+}
+
+// #endregion GetInput onClick
+
+// 
---
+++ mail.php
@@ -0,0 +1,56 @@
+<?php
+
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\SMTP;
+use PHPMailer\PHPMailer\Exception;
+
+//Load Composer's autoloader
+require 'vendor/autoload.php';
+// require 'C:\xampp\htdocs\PHPMailer\vendor\autoload.php';
+
+//Create an instance; passing `true` enables exceptions
+$mail = new PHPMailer(true);
+
+
+// Data to send Get from form.js (AJAX with Json)
+$name = $_POST['firstName'];
+$lastName = $_POST['lastName'];
+$email = $_POST['email'];
+$number = $_POST['phone'];
+$question = $_POST['message'];
+
+try {
+
+    // Email Template variables
+    $message = file_get_contents('mailTemplate.html');
+    $message = str_replace('%email%', $email, $message);
+    $message = str_replace('%name%', $name, $message);
+    $message = str_replace('%lastName%', $lastName, $message);
+    $message = str_replace('%mobile%', $number, $message);
+    $message = str_replace('%message%', $question, $message);
+
+    //Server settings
+    $mail->SMTPDebug = SMTP::DEBUG_SERVER;
+    $mail->isSMTP();
+    $mail->Host       = 'mail.impresioneslk.com';
+    $mail->SMTPAuth   = true;
+    $mail->Username   = 'info@impresioneslk.com';
+    $mail->Password   = 'Blackveil175';
+    $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;
+    $mail->Port       = 465;
+
+    //Recipients
+    $mail->setFrom('info@impresioneslk.com', 'Mailer LAIA');
+    $mail->addAddress('iku@abogadoericprice.com', 'Ivy Ku Flores');
+    $mail->addAddress('avelazquez2873@LosAngelesImmigration.onmicrosoft.com', 'Alberto Velazquez');
+
+    //Content
+    $mail->isHTML(true);
+    $mail->Subject = 'Someone has opted in to form AEP Google PPC';
+    $mail->msgHTML($message);    
+    $mail->AltBody = 'Sending email';
+
+    $mail->send();
+} catch (Exception $e) {
+    echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
+}
