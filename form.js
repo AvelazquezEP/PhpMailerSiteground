bf6de04f55029d5325f70df5371c05c1ae842e40
Alberto <avelazquez@abogadoericprice.com>
2023-03-27 17:22:37 +0000

    Fix: Styles(tailwindcss) redirects for spanish and english sites

--- form.js
+++ form.js
@@ -7,48 +7,17 @@ const sendData = () => {
 
     $.ajax({
         type: 'POST',
-        url: 'mail.php',
+        // url: 'mail.php',
+        url: 'https://ericp138.sg-host.com/mail.php',
         data: {
             firstName: firstNamePhp,
             lastName: lastNamePhp,
             phone: phonePhp,
             email: emailPhp,
             message: messagePhp,
-
         },
         success: function () {
-            console.log("Enviado pa");
+            // console.log("Enviado pa");
         }
     });
 }
-
-// #region GetInput Onclick
-
-const getFirstName = () => {
-    const first_name = document.getElementById("first_name").value;
-    document.getElementById("first_namePhp").value = first_name;
-}
-
-const getLastName = () => {
-    const last_name = document.getElementById("last_name").value;
-    document.getElementById("last_namePhp").value = last_name;
-}
-
-const getEmail = () => {
-    const email = document.getElementById("email").value;
-    document.getElementById("emailPhp").value = email;
-}
-
-const getNumber = () => {
-    const mobile = document.getElementById("mobile").value;
-    document.getElementById("mobilePhp").value = mobile;
-}
-
-const getQuestion = () => {
-    const message = document.getElementById("message").value;
-    document.getElementById("messagePhp").value = message;
-}
-
-// #endregion GetInput onClick
-
-// 
