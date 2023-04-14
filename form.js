8273d27226fae799645ee1d7aa09b849b6dcc7fa
Alberto <avelazquez@abogadoericprice.com>
2023-04-14 15:29:59 +0000

    Fix: remove the old action, now redirect to the salesforce.php this its the new file to integrate the salesforce with the site but needs change this file for other later.

--- form.js
+++ form.js
@@ -15,41 +15,28 @@ const sendData = () => {
     const phone = "VID_CONFERENCE";
     const person = "OUR_LOCATION";
 
-    // WEB TO LEAD
-    // sendToWebToLead = (oid, leadSource, language, firstNamePhp, lastNamePhp, phonePhp, emailPhp, location, sms);
+    // FORMDATA
+    const formData = new FormData();
+
+    formData.append("username", "Groucho");
+    formData.append("accountnum", 123456); // number 123456 is immediately converted to a string "123456"
+
+    // HTML file input, chosen by user
+    formData.append("userfile", fileInputElement.files[0]);
+
+    // JavaScript file-like object
+    const content = '<q id="a"><span id="b">hey!</span></q>'; // the body of the new file…
+    const blob = new Blob([content], { type: "text/xml" });
+
+    formData.append("webmasterfile", blob);
+
+    const request = new XMLHttpRequest();
+    request.open("POST", "http://foo.com/submitform.php");
+    request.send(formData);
+
 
     // SUMO SCHEDULER APP
     sendToPHP(language, firstNamePhp, lastNamePhp, phonePhp, emailPhp, comment, location, phone);
-    // $.ajax({
-    //     type: 'POST',
-    //     url: 'mail.php',
-    //     data: {
-    //         "00N5f00000SB1Ws": language,
-    //         first_name: firstNamePhp,
-    //         last_name: lastNamePhp,
-    //         mobile: phonePhp,
-    //         email: emailPhp,
-    //         message: comment,
-    //         "00N5f00000SB1X0": location,
-    //         meetingType: phone,
-    //     },
-    //     dataType: 'text',
-    //     success: function (data) {
-    //         alert(data);
-    //     }
-    // });
-
-    // if (document.getElementById("meetingTypePhone").checked) {
-
-    //     const phone = "VID_CONFERENCE";
-    //     sendToPHP(language, firstNamePhp, lastNamePhp, phonePhp, emailPhp, comment, location, phone);
-
-    // } else if (document.getElementById("meetingTypePerson").checked) {
-
-    //     const person = "OUR_LOCATION";
-    //     sendToPHP(language, firstNamePhp, lastNamePhp, phonePhp, emailPhp, comment, location, person);
-    // }
-
 }
 
 const sendToPHP = (languages, name, lastName, phone, mail, comment, place, type) => {
@@ -98,3 +85,38 @@ const sendToWebToLead = (oid, leadSource, language, firstNamePhp, lastNamePhp, p
     });
 
 }
+
+// #region HTTPS REQUEST
+
+ // Primer request
+    // const formElement = document.querySelector("form");
+    // var http = new XMLHttpRequest();
+    // var url = 'get_data.php';
+    // var params = 'orem=ipsum&name=binny';
+    // http.open('POST', url, true);
+
+    //Send the proper header information along with the request
+    // http.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
+
+    // http.onreadystatechange = function () {//Call a function when the state changes.
+    //     if (http.readyState == 4 && http.status == 200) {
+    //         alert(http.responseText);
+    //     }
+    // }
+    // http.send(params);
+
+
+// Second Request
+    // var params = new Object();
+    // params.myparam1 = myval1;
+    // params.myparam2 = myval2;
+
+    // // Turn the data object into an array of URL-encoded key/value pairs.
+    // let urlEncodedData = "", urlEncodedDataPairs = [], name;
+    // for (name in params) {
+    //     urlEncodedDataPairs.push(encodeURIComponent(name) + '=' + encodeURIComponent(params[name]));
+    // }
+
+
+
+// #endrregion HTTPS REQUEST
