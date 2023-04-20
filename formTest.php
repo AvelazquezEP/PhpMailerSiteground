958745e68c8799f9f9fce73ceef7e74720f613a3
Alberto <avelazquez@abogadoericprice.com>
2023-04-20 17:22:23 +0000

    Update: create the comments to the different process in formTest.php

--- formTest.php
+++ formTest.php
@@ -14,7 +14,6 @@ header("Expires: Mon, 26 Jul 2024 05:00:00 GMT"); //Update before 26/Jul/2024
 // Send a email
 function sendEmail($language, $email, $name, $lastName, $number, $question)
 {
-    //Create an instance; passing `true` enables exceptions
     $mail = new PHPMailer(true);
     // Email Template
     $message = file_get_contents('mailTemplate.html');
@@ -26,9 +25,8 @@ function sendEmail($language, $email, $name, $lastName, $number, $question)
     $message = str_replace('%message%', $question, $message);
 
     //Server settings
-    // $mail->SMTPDebug = SMTP::DEBUG_SERVER;
+    // $mail->SMTPDebug = SMTP::DEBUG_SERVER; <-- imprime todos los pasos que realiza el proceso de enviar correo
     $mail->isSMTP();
-
     $mail->Host       = 'smtp.office365.com';
     $mail->SMTPAuth   = true;
     $mail->Username   = 'support56@abogadoericprice.com';
@@ -36,8 +34,8 @@ function sendEmail($language, $email, $name, $lastName, $number, $question)
     $mail->SMTPSecure = 'tls';
     $mail->Port       = 587;
 
-    //Recipients
-    $mail->setFrom('support56@abogadoericprice.com'); //Correo saliente
+    //Correo saliente
+    $mail->setFrom('support56@abogadoericprice.com');
 
     // Correos a quienes le llegan
     // $mail->addAddress('iku@abogadoericprice.com', 'Ivy Ku Flores');
@@ -47,9 +45,9 @@ function sendEmail($language, $email, $name, $lastName, $number, $question)
     $mail->isHTML(true);
     $mail->Subject = 'Someone has opted in to form AEP Google PPC';
     $mail->msgHTML($message); //Toma el template(mailTemplate.html) para construtir el contenido del correo
-    $mail->AltBody = 'Sending email';
+    $mail->AltBody = 'Sending email'; // <-- Esta linea solo funciona para algun mensaje / NO SE UTILIZA puede quedar asi o comentada
 
-    // Enviar correo
+    // Toma todos los parametros anteriorres y realiza el envio del correo
     $mail->send();
 }
 
@@ -62,8 +60,8 @@ function getLocation($location)
     $SDCode = "a1b5f000000eT8bAAE";
     $SMCode = "a1b5f000000eT8gAAE";
     $CHCode = "a1b5f000000enBnAAI";
-    // $NCode = ""; TODAVIA NO HAY CODIGO
-    // $SBCode = ""; TODAVIA NO HAY CODIGO
+    // $NCode = ""; no tiene codigo ya que se registra como un APPOINTMENT VIRTUAL
+    // $SBCode = ""; NO HAY CODIGO/ID
 
     switch ($location) { //IN-PERSON (Falta san berdandino)
         case "Los Angeles":
@@ -82,7 +80,6 @@ function getLocation($location)
             $code = strval($CHCode);
             break;
         case "National":
-            // $code = $LACode;
             // $code = strval($NCode);
             break;
         case "San Bernardino":
@@ -96,9 +93,10 @@ function getLocation($location)
     return $code;
 }
 
-// Build the link for InPerson appointment
+// Build the link for In-Person appointment
 function redirectInPerson($loctionType, $locationCode, $name, $lastName, $email, $number, $location, $language, $sms)
 {
+    // El de la linea (Https://...) hace uso de un link para las citas en VIRTUAL (NATIONAL es el unico que tiene cita virtual)
     $personLink = "https://greencardla.my.site.com/s/onlinescheduler?processId=a1h5f000000nAJCAA2&locationtype=" . $loctionType
         . "&WhatId=a1n5f0000006fzTAAQ&WhereID=" . $locationCode
         . "&sumoapp_WhoId=0055f000007NE9T"
@@ -115,6 +113,7 @@ function redirectInPerson($loctionType, $locationCode, $name, $lastName, $email,
 // Build the link for Virtual appointment
 function redirectVirtual($loctionType, $locationCode, $name, $lastName, $email, $number, $location, $language, $sms)
 {
+    // El de la linea (Https://...) hace uso de un link para las citas en PERSON (TODOS EXCEPTO NATIONAL tienen cita IN-PERSON)
     $redirectLink = "https://greencardla.my.site.com/s/onlinescheduler?processId=a1h5f000000nAJZAA2&locationtype=" . $loctionType
         . "&WhatId=a1n5f0000006fzTAAQ&WhereID=" . $locationCode
         . "&sumoapp_WhoId=0055f000007NE9T"
@@ -124,10 +123,11 @@ function redirectVirtual($loctionType, $locationCode, $name, $lastName, $email, 
         . "&a6=" . $number
         . "&a8=" . $location
         . "&a9=" . $language
-        . "&a10=" . $sms;;
+        . "&a10=" . $sms;
     return $redirectLink;
 }
 
+// Revisa el tipo de cita (hace uso de select.js) con esto puede saber cual de las dos function anteriores puede usar para Virtual o In-Person
 function getLink($meetingType, $locationCode, $name, $lastName, $email, $phoneNumber, $location, $language, $sms)
 {
     $type = "";
@@ -145,10 +145,8 @@ function getLink($meetingType, $locationCode, $name, $lastName, $email, $phoneNu
     }
 }
 
-// Location type
-
 try {
-    // Data to send Get from index.html (CONTACT FORM)
+    // Value de los inputs, estos los recibe directamente del Contact Form al enviarse por medio de un HTTP/POST en el ACTION
     $name = $_POST['first_name'];
     $lastName = $_POST['last_name'];
     $email = $_POST['email'];
@@ -159,9 +157,9 @@ try {
     $sms = $_POST['00N5f00000SB1XU'];
     $meetingType = $_POST['meetingType'];
     $oid = $_POST['oid'];
-    // $sourceLead = $_POST['sourceLead'];
+    // $sourceLead = $_POST['sourceLead']; //No se necesita porque se registra directo en SUMO SCHEDULER
 
-    // Convert inputs to Sting
+    // Toma los inputs anteriores y los convierte a STRING, esto se usa en ciertas functions.
     $strName = strval($name);
     $strlastName = strval($lastName);
     $stremail = strval($email);
@@ -171,16 +169,13 @@ try {
     $strlanguage = strval($language);
     $strsms = strval($sms);
     $strOid = strval($oid);
-    // $strSourceLead = strval($sourceLead);
+    // $strSourceLead = strval($sourceLead); //No se necesita porque se registra directo en SUMO SCHEDULER
 
     $locationCode = getLocation($location);
 
-    // WEB-TO-LEAD
+    // Send to WEB-TO-LEAD
     $url = "https://webto.salesforce.com/servlet/servlet.WebToLead?encoding=UTF-8";
-    // $data = array();
-    // 'lead_source' => $strSourceLead,
-    // '00N5f00000SB1X0' => $strlocation,
-    // '00N5f00000SB1XU' => $strsms,    
+    // Solo se usa la inforacion necesaria para la creacion del LEAD en SALESFORCE.
     $data = [
         'oid' => $strOid,
         'first_name' => $strName,
@@ -189,8 +184,7 @@ try {
         'mobile' => $strnumber
     ];
 
-    // httpPost($url, $data);
-
+    // Se usa CURL para poder realizar un envio de tipo POST al $url, tomando en cuenta como parametros el array $data
     $curl = curl_init($url);
     curl_setopt($curl, CURLOPT_POST, true);
     curl_setopt($curl, CURLOPT_POSTFIELDS, http_build_query($data));
@@ -198,23 +192,25 @@ try {
     curl_exec($curl);
     curl_close($curl);    
 
-    // Function to Send email
+    // Envia el correo con los datos obtenidos en las variables anteriores.
     sendEmail($strlanguage, $stremail, $strName, $strlastName, $strnumber, $question);
 
-    // // Get the Link (URL for redirect to the scheduler app)
+    // Obtenemos el link y lo almacenamos en una variable para poder usarlo en un Header y poder redireccionarlo
     $link = getLink($meetingType, $locationCode, $strName, $strlastName, $stremail, $strnumber, $strlocation, $strlanguage, $strsms);
 
+    // Esperamos 5s antes de poder redirigir al link.
+    // mientras pasa el tiempo declarado se moestrar una vista con informacion necesario (vista de espera o notificar que se esta procesando su informacion)
+    // De esta forma podemos dar tiempo a que el proceso de creacino del LEAD se pueda completar
     header("refresh:5; url=" . $link);
-
-    // sleep(9);
-    // header("Location: " . $link);
+    
 } catch (Exception $e) {
-    // echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
-    header("Location: https://ericp138.sg-host.com/sorry.html");    // <--- show this site when something is wrong
+    // echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}"; <-- muestra un mensaje de informacion en caso de que falle
+    header("Location: https://ericp138.sg-host.com/sorry.html");    // <--- Muestra esa vista cuando el proceso falle
 }
 
 ?>
 
+<!-- Empieza la vista que se muestra mientras se procesan los datos -->
 <!DOCTYPE html>
 <html lang="en">
 
@@ -222,7 +218,7 @@ try {
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Esperanding</title>
+    <title>Enviando información</title>
 </head>
 <body>
     <div class="container">
