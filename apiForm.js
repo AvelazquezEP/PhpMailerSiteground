5408d07b20d442ddd2ed85e4419a2fc6982e4bb2
AvelazquezSSH <avelazquez@abogadoericprice.com>
2023-06-05 22:14:07 +0000

    FIX: the way to transform the button to disable, now when we click in the button and all the input are correct the button send will convert to disable (this prevent the user can click more than one more time) and the backgrund color change to gray (this is only to know when the button is disable)

--- apiForm.js
+++ apiForm.js
@@ -22,30 +22,19 @@ $(document).ready(function () {
 });
 
 const sendData = (firstName, lastName, email, mobilePhone, location, language, meetingType, comment, sms) => {
-    // let firstName = document.getElementById("first_name").value;
-    // let lastName = document.getElementById("last_name").value;
-    // let email = document.getElementById("email").value;
-    // let mobilePhone = document.getElementById("mobile").value;
-    // let location = document.getElementById("00N5f00000SB1X0").value;
-    // let language = document.getElementById("00N5f00000SB1Ws").value;
-    // // let smsOption = document.getElementById("00N5f00000SB1XU").value;
-    // let meetingType = document.getElementById("meetingTypePerson").value;
-    // let comment = document.getElementById("message").value;
-    // let sms = document.getElementById("00N5f00000SB1XU").value;
 
-    // var mobileInput = document.getElementById('mobileInput').innerHTML = 'debe ser de 10 digitos';
     if (mobilePhone.length < 10) {
-        // log(`Tel: ${mobilePhone} NO es de 10 digitos`);
         document.getElementById('mobileInput').innerHTML = 'The phone number must be 10 digits';
         document.getElementById('mobileInput').style.color = "#F93C17";
     } else {
+        document.getElementById('ButtonSend').style.backgroundColor = 'gray';
+        document.getElementById('ButtonSend').innerHTML = 'Sending Data';
         createLeadApi(firstName, lastName, email, mobilePhone, location, language, sms, comment);
         document.getElementById('mobileInput').innerHTML = '';
-        // log(`Tel: ${mobilePhone} SI cumple con los 10 digitos`);
     }
 
     // PARA EL FUNCIONAMIENTO DEL DOBLE CLICK
-    setTimeout('$("#ButtonSend").removeAttr("disabled")', 2800);
+    setTimeout('$("#ButtonSend").removeAttr("disabled")', 3800);
 }
 
 const createLeadApi = (first_name, last_name, email, mobile_phone, location_name, language_site, sms_option, comment = "-") => {
